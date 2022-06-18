452dc2ca4f34f08f60dbc56aeaa9a6be42b6a7a0
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2022-06-18 14:58:41 -0300

    inde.d.ts gerado corretamente os exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from "./Button";
-export { default as Comment } from "./Comments";
-export { default as CommentForm } from "./Comments";
+export { default as Comment } from "./Comments/Comment";
+export { default as CommentForm } from "./Comments/CommentForm";
