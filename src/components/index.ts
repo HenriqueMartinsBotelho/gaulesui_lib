e2aa9f8bbb511058d2c4db1393e20589954c8f93
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2022-06-22 10:52:54 -0300

    Import errors

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Comment } from "./Comments/Comment";
 export { default as CommentForm } from "./Comments/CommentForm";
+export { default as SlidingPanel } from "./SlidingPanel";
